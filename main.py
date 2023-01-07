331c49a2f422102aa5f0c8889b0b2576f74e153d
Mikiko - <81929494+mikikoy@users.noreply.github.com>
2023-01-07 04:40:43 +0000

    added menu screen

--- main.py
+++ main.py
@@ -34,6 +34,145 @@ if not cursor.fetchone()[0]==1:
 cursor.execute("""SELECT * from Highscore""")
 
 sqlite_select_query = """SELECT highscore from Highscore"""
+white = (255, 255, 255)
+black = (0, 0, 0)
+gray = (193,205,205)
+
+background_colour = (255,255,255)
+X = 600
+Y = 400
+pygame.init()
+
+def instruction():
+  def button(msg,len,l,wid,w):
+    smallfont = pygame.font.SysFont('Corbel',35)
+    button = smallfont.render(msg , True , gray)
+    screen.blit(button, (X/len+l,Y/wid+w))   
+    pygame.display.update() 
+  
+  screen = pygame.display.set_mode((X, Y))
+  pygame.display.set_caption('ending')
+  screen.fill(background_colour)
+  pygame.display.flip()
+  intro_bg = pygame.image.load("instructions.png")
+  intro_bg = pygame.transform.scale(intro_bg, (X, Y))
+  
+  color_light = (170,170,170) 
+  color_dark = (100,100,100) 
+
+
+  smallfont = pygame.font.SysFont('Corbel',35) 
+    
+  # rendering a text written in 
+  # this font 
+
+  exit_button = smallfont.render('X' , True , gray)
+
+    
+  while True: 
+        
+      for ev in pygame.event.get(): 
+            
+          if ev.type == pygame.QUIT: 
+              pygame.quit() 
+                
+          #checks if a mouse is clicked 
+          if ev.type == pygame.MOUSEBUTTONDOWN: 
+                
+              #if the mouse is clicked on the 
+              # button the game is terminated 
+              if X/8.5 <= mouse[0] <= X/8.5+20 and Y/10.5 <= mouse[1] <= Y//10.5+20: 
+                  intro()
+                    
+      # fills the screen with a color 
+      screen.blit(intro_bg, (0, 0))
+        
+      # stores the (x,y) coordinates into 
+      # the variable as a tuple 
+      mouse = pygame.mouse.get_pos() 
+        
+      # if mouse is hovered on a button it 
+      # changes to lighter shade 
+      if X/8.5 <= mouse[0] <= X/8.5+20 and Y/10.5 <= mouse[1] <= Y/10.5+20: 
+            pygame.draw.rect(screen,color_light,[X/8.5,Y/10.5,20,20]) 
+            
+      else: 
+          pygame.draw.rect(screen,color_dark,[X/8.5,Y/10.5,20,20]) 
+        
+      # superimposing the text onto our button 
+      button("x",8.5,3,10.5,-2.5)
+        
+      # updates the frames of the game 
+      pygame.display.update() 
+
+def intro():
+  screen = pygame.display.set_mode((X, Y))
+  pygame.display.set_caption('main menu')
+  screen.fill(background_colour)
+  pygame.display.flip()
+  intro_bg = pygame.image.load("intro.png")
+  intro_bg = pygame.transform.scale(intro_bg, (X, Y))
+  
+  color_light = (170,170,170) 
+  color_dark = (100,100,100) 
+
+  smallfont = pygame.font.SysFont('Corbel',35) 
+    
+  # rendering a text written in 
+  # this font 
+  
+  quit_text = smallfont.render('quit' , True , gray) 
+  play_text = smallfont.render('play' , True , gray)
+  instruction_text = smallfont.render('how to play' , True , gray)
+    
+  while True: 
+        
+      for ev in pygame.event.get(): 
+            
+          if ev.type == pygame.QUIT: 
+              pygame.quit() 
+                
+          #checks if a mouse is clicked 
+          if ev.type == pygame.MOUSEBUTTONDOWN: 
+                
+              #if the mouse is clicked on the 
+              # button the game is terminated 
+              if X/3 <= mouse[0] <= X/3+200 and Y/3 <= mouse[1] <= Y/3+40: 
+                  play()
+              elif X/3 <= mouse[0] <= X/3+200 and Y/2 <= mouse[1] <= Y/2+40: 
+                  instruction()
+                
+              elif X/3 <= mouse[0] <= X/3+200 and Y/1.5 <= mouse[1] <= Y/1.5+40: 
+                pygame.quit() 
+                    
+      # fills the screen with a color 
+      screen.blit(intro_bg, (0, 0))
+        
+      # stores the (x,y) coordinates into 
+      # the variable as a tuple 
+      mouse = pygame.mouse.get_pos() 
+        
+      # if mouse is hovered on a button it 
+      # changes to lighter shade 
+      if X/3 <= mouse[0] <= X/3+200 and Y/3 <= mouse[1] <= Y/3+40: 
+            pygame.draw.rect(screen,color_light,[X/3,Y/3,200,40]) 
+      elif X/3 <= mouse[0] <= X/3+200 and Y/2 <= mouse[1] <= Y/2+40: 
+          pygame.draw.rect(screen,color_light,[X/3,Y/2,200,40]) 
+      elif X/3 <= mouse[0] <= X/3+200 and Y/1.5 <= mouse[1] <= Y/1.5+40: 
+          pygame.draw.rect(screen,color_light,[X/3,Y/1.5,200,40]) 
+            
+      else: 
+          pygame.draw.rect(screen,color_dark,[X/3,Y/3,200,40]) 
+          pygame.draw.rect(screen,color_dark,[X/3,Y/2,200,40]) 
+          pygame.draw.rect(screen,color_dark,[X/3,Y/1.5,200,40]) 
+        
+      # superimposing the text onto our button 
+      screen.blit(play_text, (X/2-20,Y/3))   
+      screen.blit(instruction_text, (X/2-65,Y/2)) 
+      screen.blit(quit_text, (X/2-20,Y/1.5)) 
+        
+      # updates the frames of the game 
+      pygame.display.update() 
 
 def play():
   def textbox(msg, width, length, color): 
@@ -159,7 +298,7 @@ def play():
             elif evt.key == K_BACKSPACE:
               guess = guess[:-1]
             elif evt.key == K_RETURN:
-                if guess == word:
+                if guess.lower() == word:
                   point += 1
                   guess = "click space to edit text..."
                   guessing = False
@@ -197,4 +336,4 @@ def play():
       pygame.display.flip()
       pygame.display.update()
 
-play()
+intro()
